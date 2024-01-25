997eb546917b3088ec37dcfda6b0ede337dacefd
Max Suica <Max.Suica@gmail.com>
2024-01-25 08:44:09 -0800

    Basic Syntax / Parser / Dumper
    * Syntax class Hierarchy:
    
    * Node
      * Expression
      * Sequence
      * Map
        * Tag
        * If
        * A
    * Syntax
      * Simple Syntax
    
    
    *  Parser module
       * parse_popo function
       * parse_yaml function
       * dump_popo function
       * dump_yamp function
    * NotRecognized Exception
    
    * Testings
       * test_parser module
       * test_syntax module

---
+++ src/engine/syntax.py
@@ -0,0 +1,121 @@
+from dataclasses import dataclass, replace
+from typing import Any
+
+# Base Nodes ------------------------------------------------------------------
+
+
+class Node:
+    @property
+    def type(self):
+        return self.__class__.__name__
+
+
+NodeType = type[Node]
+NodeTypes = tuple[NodeType]
+
+
+@dataclass
+class Expression(Node):
+    data: str
+    pattern: str = ".*"
+
+
+ExpressionType = type[Expression]
+ExpressionTypes = tuple[ExpressionType]
+
+
+@dataclass
+class Sequence(Node):
+    data: list
+
+
+SequenceType = type[Sequence]
+SequenceTypes = tuple[SequenceType]
+
+
+@dataclass
+class Tag:
+    key: str
+    type: Node
+    optional: bool = False
+
+
+Tags = list[Tag]
+
+
+@dataclass
+class Map(Node):
+    data: dict
+    spec: Tags
+
+    def __getitem__(self, key: str) -> Any:
+        return self.data[key]
+
+
+MapType = type[Map]
+MapTypes = tuple[MapType]
+
+
+# Syntax ----------------------------------------------------------------------
+
+
+@dataclass
+class Syntax:
+    types: NodeTypes
+
+    @property
+    def expressions(self) -> ExpressionTypes:
+        return self.by_type(Expression)
+
+    @property
+    def sequences(self) -> SequenceTypes:
+        return self.by_type(Sequence)
+
+    @property
+    def maps(self) -> MapTypes:
+        return self.by_type(Map)
+
+    def by_type(self, target_type: NodeType) -> NodeTypes:
+        return [t for t in self.types if issubclass(t, target_type)]
+
+    def extend(self, *new_types: NodeTypes) -> "Syntax":
+        return replace(self, types=self.types + list(new_types))
+
+
+# Initial syntax --------------------------------------------------------------
+
+
+empty_syntax = Syntax(types=[])
+
+initial_syntax = Syntax(types=[Expression, Sequence])
+
+
+# Basic Syntax ----------------------------------------------------------------
+
+
+@dataclass
+class A(Map):
+    spec: Tags = (Tag("a", Expression),)
+
+
+@dataclass
+class If(Map):
+    spec: Tags = (
+        Tag("if", Expression),
+        Tag("then", Sequence),
+        Tag("else", Sequence, optional=True),
+    )
+
+
+simple_syntax = initial_syntax.extend(If, A)
+
+
+# Syntax v1.0 -----------------------------------------------------------------
+
+
+@dataclass
+class Variable(Expression):
+    pattern: str = "^[a-zA-Z_][a-zA-Z0-9_]*$"
+
+
+syntax_v1 = simple_syntax.extend(Variable)

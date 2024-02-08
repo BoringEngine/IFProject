7405a295fc25940ce5daa087ce1dd57948656e21
Max Suica <Max.Suica@gmail.com>
2024-02-08 14:39:44 -0800

    Give Interpreter a mock `choices` attribute
    This lets the interpreter spam the view with choices, which is our current scenario

--- src/engine/interpreter.py
+++ src/engine/interpreter.py
@@ -5,13 +5,16 @@ class Interpreter:
     def __init__(self):
         dispatcher.connect(self.handle_choice, signal="Make_Choice")
 
+        # Mock choices: True is a placeholder for Address
+        self.choices = {"north": True, "south": True, "east": True, "west": True}
+
     def run(self):
         # Keep running steps until we return False
         # (meaning we should wait for user input)
         while self.step():
             pass
 
-        dispatcher.send("Give_Choice", self, choices=self.choices)
+        dispatcher.send("Give_Choice", self, self.choices)
 
     def handle_choice(self, sender, choice: str):
         self.address = self.choice_to_address(choice)
